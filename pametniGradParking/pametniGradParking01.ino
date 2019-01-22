b356665c30fcd0ddedd9fc5ec0ddf04a8f9424a2
Lovro Šverko <lovro.sverko@sser.hr>
2019-01-22 20:04:33 +0100

    Update pametniGradParking01.ino

--- pametniGradParking/pametniGradParking01.ino
+++ pametniGradParking/pametniGradParking01.ino
@@ -1,7 +1,9 @@
 /*
 Projekt: Pametni grad
 Tim: Pametni parking
-Autor: Adrian Prodan, 1.c
+Autori: 
+	Adrian Prodan, 1.c
+	Bernard Miculinić, 1.c
 Srednja škola za elektrotehniku i računalstvo, Rijeka
 Školska godina 2018./2019.
 */
